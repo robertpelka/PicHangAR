4df339370773a5de842110f179cf51494a776f15
Robert Pelka <123455ff@gmail.com>
2021-03-28 23:51:59 +0200

    Add detecting vertical planes and hanging simple boxes on touch

--- PicHangAR/ViewController.swift
+++ PicHangAR/ViewController.swift
@@ -16,59 +16,68 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.debugOptions = [.showFeaturePoints]
+        sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
+        configuration.planeDetection = .vertical
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: sceneView)
+            guard let query = sceneView.raycastQuery(from: touchLocation, allowing: .existingPlaneInfinite, alignment: .vertical) else {return}
+            let results = sceneView.session.raycast(query)
+            if let hitResult = results.first {
+                hangPicture(atLocation: hitResult)
+            }
+        }
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+    func hangPicture(atLocation hitResult: ARRaycastResult) {
+        let frame = SCNBox(width: 0.15, height: 0.2, length: 0.015, chamferRadius: 0.003)
+        frame.firstMaterial?.diffuse.contents = UIColor.red
+        let frameNode = SCNNode(geometry: frame)
+        guard let hitResultAnchor = hitResult.anchor else {return}
+        frameNode.transform = SCNMatrix4(hitResultAnchor.transform)
+        frameNode.eulerAngles.x -= (.pi / 2)
+        let location = hitResult.worldTransform.columns.3
+        frameNode.position = SCNVector3(
+            x: location.x,
+            y: location.y,
+            z: location.z)
+        sceneView.scene.rootNode.addChildNode(frameNode)
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+    //MARK: - ARSCNViewDelegateMethods
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        let planeNode = createPlane(withPlaneAnchor: planeAnchor)
+        node.addChildNode(planeNode)
     }
+    
+    func createPlane(withPlaneAnchor planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+        let planeNode = SCNNode()
+        planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+        planeNode.geometry = plane
+        print("Plane detected")
+        return planeNode
+    }
+    
 }
