0aa6890e5b2ae405fe5d89bcee482c3998d6f596
Robert Pelka <123455ff@gmail.com>
2021-04-01 18:58:52 +0200

    Add choosing a picture and inserting it into a frame

--- PicHangAR/ViewController.swift
+++ PicHangAR/ViewController.swift
@@ -14,14 +14,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet var sceneView: ARSCNView!
     @IBOutlet weak var scanningLabel: UILabel!
     @IBOutlet weak var foundLabel: UILabel!
+    @IBOutlet weak var choosePictureButton: UIButton!
+    
+    let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        
+        choosePictureButton.imageView?.contentMode = .scaleAspectFill
+        choosePictureButton.layer.borderWidth = 4
+        choosePictureButton.layer.borderColor = UIColor.white.cgColor
+        choosePictureButton.layer.cornerRadius = 15
+        
         scanningLabel.layer.masksToBounds = true
         scanningLabel.layer.cornerRadius = scanningLabel.frame.height / 2
         scanningLabel.alpha = CGFloat(0)
-        flash(label: scanningLabel)
+        animateFlash(label: scanningLabel)
         foundLabel.layer.masksToBounds = true
         foundLabel.layer.cornerRadius = scanningLabel.frame.height / 2
         
@@ -31,14 +42,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         registerGestureRecognizers()
     }
     
-    func flash(label: UILabel) {
+    func animateFlash(label: UILabel) {
         UIView.animate(withDuration: 0.8, delay: 0, options: [.repeat, .autoreverse], animations: {
             label.alpha = CGFloat(1)
         }, completion: nil)
     }
     
-    func fadeOut(label: UILabel) {
-        UIView.animate(withDuration: 0.4, delay: 0.8, options: [], animations: {
+    func animatefadeOut(label: UILabel) {
+        UIView.animate(withDuration: 0.4, delay: 0.4, options: [], animations: {
             label.alpha = CGFloat(0)
         }, completion: nil)
     }
@@ -68,6 +79,40 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         if let result = results.first {
             hangPicture(atLocation: result)
         }
+    }
+    
+    func hangPicture(atLocation hitResult: ARRaycastResult) {
+        let choosenPicture = choosePictureButton.image(for: .normal)
+        let pictureAspectRatio = (choosenPicture?.size.width ?? 4) / (choosenPicture?.size.height ?? 3)
+        let picture = SCNPlane(width: 0.2*pictureAspectRatio, height: 0.2)
+        picture.firstMaterial?.diffuse.contents = choosePictureButton.image(for: .normal)
+        let pictureNode = SCNNode(geometry: picture)
+        
+        let frameThickness: CGFloat = 0.05
+        let frameDepth: CGFloat = 0.016
+        let frame = SCNBox(width: picture.width+frameThickness, height: picture.height+frameThickness, length: frameDepth, chamferRadius: 0.003)
+        frame.firstMaterial?.diffuse.contents = UIColor.white
+        let frameNode = SCNNode(geometry: frame)
+        
+        guard let hitResultAnchor = hitResult.anchor else {return}
+        frameNode.transform = SCNMatrix4(hitResultAnchor.transform)
+        frameNode.eulerAngles.x -= (.pi / 2)
+        
+        let location = hitResult.worldTransform.columns.3
+        frameNode.position = SCNVector3(
+            x: location.x,
+            y: location.y,
+            z: location.z + Float(frameDepth/2)
+        )
+        
+        frameNode.addChildNode(pictureNode)
+        
+        pictureNode.position = SCNVector3(
+            x: 0,
+            y: 0,
+            z: Float(frameDepth/2)+0.001)
+        
+        sceneView.scene.rootNode.addChildNode(frameNode.flattenedClone())
     }
     
     func indicateSelection(ofNode node: SCNNode) {
@@ -128,22 +173,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    func hangPicture(atLocation hitResult: ARRaycastResult) {
-        let frame = SCNBox(width: 0.15, height: 0.2, length: 0.015, chamferRadius: 0.003)
-        frame.firstMaterial?.diffuse.contents = UIColor.red
-        let frameNode = SCNNode(geometry: frame)
-        guard let hitResultAnchor = hitResult.anchor else {return}
-        frameNode.transform = SCNMatrix4(hitResultAnchor.transform)
-        frameNode.eulerAngles.x -= (.pi / 2)
-        let location = hitResult.worldTransform.columns.3
-        frameNode.position = SCNVector3(
-            x: location.x,
-            y: location.y,
-            z: location.z)
-        sceneView.scene.rootNode.addChildNode(frameNode)
-    }
-    
-    //MARK: - ARSCNViewDelegateMethods
+    //MARK: - ARSCNViewDelegate
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         DispatchQueue.main.async {
@@ -151,7 +181,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 scnLabel.removeFromSuperview()
             }
             self.foundLabel.isHidden = false
-            self.fadeOut(label: self.foundLabel)
+            self.foundLabel.alpha = CGFloat(1)
+            self.animatefadeOut(label: self.foundLabel)
         }
         guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
         let planeNode = createPlane(withPlaneAnchor: planeAnchor)
@@ -167,6 +198,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         planeNode.geometry = plane
         print("Plane detected")
         return planeNode
+    }
+    
+}
+
+//MARK: - UIImagePickerControllerDelegate, UINavigationControllerDelegate
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBAction func choosePictureButtonPressed(_ sender: UIButton) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let choosenImage = info[.originalImage] as? UIImage {
+            choosePictureButton.setImage(choosenImage, for: .normal)
+            imagePicker.dismiss(animated: true, completion: nil)
+        }
     }
     
 }
